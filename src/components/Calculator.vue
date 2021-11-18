7e97f6a87c085effaeb260924be9abf3de693b2f
albertosetim <albertosetim@gmail.com>
2021-11-18 03:02:06 +0100

    remake

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,25 +1,32 @@
 <template>
   <div class="calculator">
-      <div class="display border-top border-left border-right">{{ current }}</div>
-      <div class="btn border-left border-top border-right" @click="clear()" >AC</div>
+      <div class="display border-top border-left border-right">
+        <ul>
+          <li v-for=" ( item, index ) in history" :key="index">
+              
+          </li>
+        </ul>
+        {{ current_operation }} {{current_result}}
+      </div>
+      <div class="btn border-left border-top border-right" @click="clear()">C</div>
       <div class="btn border-top border-right" >+/-</div>
-      <div class="btn border-top border-right" @click="percent">%</div>
-      <div class="btn operator  " @click="divide">/</div>
-      <div class="btn border-left border-top border-right" @click="append('7')" >7</div>
-      <div class="btn border-top border-right" @click="append('8')" >8</div>
-      <div class="btn border-top border-right" @click="append('9')" >9</div>
-      <div class="btn operator border-top" @click="times" >x</div>
-      <div class="btn border-left border-top border-right" @click="append('4')" >4</div>
-      <div class="btn border-top border-right" @click="append('5')" >5</div>
-      <div class="btn border-top border-right" @click="append('6')" >6</div>
-      <div class="btn operator border-top" @click="minus" >-</div>
-      <div class="btn border-left border-top border-right" @click="append('1')" >1</div>
-      <div class="btn border-top border-right" @click="append('2')" >2</div>
-      <div class="btn border-top border-right" @click="append('3')" >3</div>
-      <div class="btn operator border-top" @click="add" >+</div>
-      <div class="btn zero border-left border-top border-right border-bottom" @click="append('0')" >0</div>
-      <div class="btn border-top border-right border-bottom" @click="dot">.</div>
-      <div class="btn operator border-top border-bottom" @click="equal">=</div>
+      <div class="btn border-top border-right" @click="operator('%')" >%</div>
+      <div class="btn operator " @click="operator('/')" >/</div>
+      <div class="btn border-left border-top border-right" @click="number('7')">7</div>
+      <div class="btn border-top border-right" @click="number('8')">8</div>
+      <div class="btn border-top border-right" @click="number('9')">9</div>
+      <div class="btn operator border-top" @click="operator('*')">x</div>
+      <div class="btn border-left border-top border-right" @click="number('4')">4</div>
+      <div class="btn border-top border-right"  @click="number('5')">5</div>
+      <div class="btn border-top border-right"  @click="number('6')">6</div>
+      <div class="btn operator border-top"  @click="operator('-')" >-</div>
+      <div class="btn border-left border-top border-right"  @click="number('1')">1</div>
+      <div class="btn border-top border-right"  @click="number('2')">2</div>
+      <div class="btn border-top border-right"  @click="number('3')">3</div>
+      <div class="btn operator border-top" @click="operator('+')" >+</div>
+      <div class="btn zero border-left border-top border-right border-bottom" @click="number('0')">0</div>
+      <div class="btn border-top border-right border-bottom">.</div>
+      <div class="btn operator border-top border-bottom">=</div>
   </div>
 </template>
 
@@ -28,21 +35,84 @@
     name: 'Calculator',
     data() {
       return {
-        beggin: true,
-        previous: null,
-        current:'0',
-        operator: null,
-        operatorClicked: false
+        current_operation : '',
+        current_result : '',
+        current_operator : '',
+        history : [],
+        result  : '',
+        preview : '' 
       }
     },
     methods: {
 
-      clear()
+      
+      /* Add */
+      add(a, b) 
       {
-        this.current = '0';
-        this.beggin = true;
+        this.current_result = parseFloat( a ) + parseFloat( b );
       },
 
+      /* Operator pressed */
+
+      operator(op)
+      {
+        if(this.current_operation)
+        {
+          this.current_operation = this.current_operation + ' ' + op + ' ';
+          this.current_operator = op;
+        }
+      },
+  
+      /* Number pressed */
+
+      number( number )
+      {
+        if(this.current_operation)
+        {
+          this.current_operation = this.current_operation + number;
+          this.current_result = eval(this.current_operation);
+        }
+        else
+        {
+          this.current_operation = number;
+        }
+
+        
+      },
+
+      /* Print the history */
+
+      print_history () 
+      {
+
+      },
+
+      /* Print the screen preview */
+
+      print_preview () 
+      {
+
+      },
+
+
+      /* Clear screen and reset to zero ( "0" ) */
+
+      clear()
+      {
+        this.current_operation = '';
+        this.current_result = '0';
+      },
+
+
+      /* resolve the string */
+      resolve_multiplication()
+      {
+
+      }
+
+
+
+/*  
       percent()
       {
         this.current = `${parseFloat(this.current)/ 100}`;
@@ -101,7 +171,7 @@
           this.current = `${ this.operator( parseFloat (this.previous), parseFloat(this.current))}`;
           this.previous = null;
       }
-
+ */
     },
   }
 </script>
@@ -109,12 +179,10 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-
   .operator
   {
     background: rgb(47, 47, 71);
     color: white;
-    
   }
 
   .calculator
